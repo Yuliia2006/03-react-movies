9fcf3b5e99f40ce7a8b38a660f75b1403861cc21
Yuliia Krasnozhon <ykrasnozhen@ukr.net>
2025-08-02 16:01:53 +0300

    fixed files

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,7 +6,7 @@ import Loader from '../components/Loader/Loader';
 import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
 import MovieModal from '../components/MovieModal/MovieModal';
 import { fetchMovies } from '../services/movieService';
-import { Movie } from '../types/movie';
+import type { Movie } from '../types/movie';
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
